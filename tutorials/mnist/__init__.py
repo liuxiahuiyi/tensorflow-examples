a63eea9c92303d971decbe36a05addba239d5f9a
wangyike1 <wangyike@jd.com>
2017-12-12 21:05:25 +0800

    TG-[feature]添加words2vec

--- tutorials/mnist/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Imports mnist tutorial libraries used by tutorial examples."""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from tensorflow.examples.tutorials.mnist import input_data
-from tensorflow.examples.tutorials.mnist import mnist
--- tutorials/mnist/mnist.py
+++ tutorials/mnist/mnist.py
@@ -39,8 +39,10 @@ def inference(images, hidden1_units, hidden2_units):
         tf.truncated_normal([IMAGE_PIXELS, hidden1_units],
                             stddev=1.0 / math.sqrt(float(IMAGE_PIXELS))),
         name='weights')
+    tf.summary.histogram('weights', weights)
     biases = tf.Variable(tf.zeros([hidden1_units]),
                          name='biases')
+    tf.summary.histogram('biases', biases)
     hidden1 = tf.nn.relu(tf.matmul(images, weights) + biases)
   # Hidden 2
   with tf.name_scope('hidden2'):
@@ -48,8 +50,10 @@ def inference(images, hidden1_units, hidden2_units):
         tf.truncated_normal([hidden1_units, hidden2_units],
                             stddev=1.0 / math.sqrt(float(hidden1_units))),
         name='weights')
+    tf.summary.histogram('weights', weights)
     biases = tf.Variable(tf.zeros([hidden2_units]),
                          name='biases')
+    tf.summary.histogram('biases', biases)
     hidden2 = tf.nn.relu(tf.matmul(hidden1, weights) + biases)
   # Linear
   with tf.name_scope('softmax_linear'):
@@ -57,8 +61,10 @@ def inference(images, hidden1_units, hidden2_units):
         tf.truncated_normal([hidden2_units, NUM_CLASSES],
                             stddev=1.0 / math.sqrt(float(hidden2_units))),
         name='weights')
+    tf.summary.histogram('weights', weights)
     biases = tf.Variable(tf.zeros([NUM_CLASSES]),
                          name='biases')
+    tf.summary.histogram('biases', biases)
     logits = tf.matmul(hidden2, weights) + biases
   return logits
 
